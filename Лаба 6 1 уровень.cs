88bcd781a80b7882779972f0733f7233a0f8ae1c
KirillNaumov234 <162305953+KirillNaumov234@users.noreply.github.com>
2024-04-16 11:27:00 +0300

    Update Лаба 6 1 уровень.cs

--- Лаба 6 1 уровень.cs
+++ Лаба 6 1 уровень.cs
@@ -2,15 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-// Определяем класс Participant для хранения информации об участниках
 class Participant
 {
     public string Surname { get; set; }
     public string Society { get; set; }
     public double Attempt1Result { get; set; }
     public double Attempt2Result { get; set; }
-
-    // Метод для получения суммы результатов двух попыток
     public double GetTotalResult()
     {
         return Attempt1Result + Attempt2Result;
@@ -21,7 +18,6 @@ class Program
 {
     static void Main()
     {
-        // Список участников
         List<Participant> participants = new List<Participant>
         {
             new Participant { Surname = "Иванов", Society = "Общество 1", Attempt1Result = 6.0, Attempt2Result = 6.5 },
@@ -31,14 +27,11 @@ class Program
             new Participant { Surname = "Смирнов", Society = "Общество 5", Attempt1Result = 8.0, Attempt2Result = 8.5 }
         };
 
-        // Сортировка участников по сумме результатов двух попыток
         participants.Sort((x, y) => x.GetTotalResult().CompareTo(y.GetTotalResult()));
 
-        // Вывод заголовка таблицы
         Console.WriteLine("| Фамилия | Общество | Попытка 1 | Попытка 2 | Сумма |");
         Console.WriteLine("|---------|----------|-----------|-----------|-------|");
 
-        // Вывод результатов участников в виде таблицы
         foreach (var participant in participants)
         {
             Console.WriteLine($"| {participant.Surname} | {participant.Society} | {participant.Attempt1Result} | {participant.Attempt2Result} | {participant.GetTotalResult()} |");
