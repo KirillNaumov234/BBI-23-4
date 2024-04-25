688eb0046bfab8ade437adcfac315d84b8e59ed1
KirillNaumov234 <162305953+KirillNaumov234@users.noreply.github.com>
2024-04-25 20:49:58 +0300

    Update лаба6.3.cs

--- лаба6.3.cs
+++ лаба6.3.cs
@@ -4,56 +4,49 @@ using System.Linq;
 
 class RadioSurvey
 {
+    class SurveyQuestion
+    {
+        public string Question { get; set; }
+        public List<string> TopAnswers { get; set; }
+    }
+
     static void Main()
     {
         List<List<string>> responses = new List<List<string>>
         {
-            new List<string> { "Кошка", "Собака", "Кошка", "Рыба" }, 
-            new List<string> { "Дружелюбие", "Упорство", "Трудолюбие", "Скромность" }, 
-            new List<string> { "Сакура", "Самурай", "Суши", "Саке", "Сумо" } 
+            new List<string> { "Кошка", "Собака", "Кошка", "Рыба" },
+            new List<string> { "Дружелюбие", "Упорство", "Трудолюбие", "Скромность" },
+            new List<string> { "Сакура", "Самурай", "Суши", "Саке", "Сумо" }
         };
 
-        var topAnswers = new List<List<string>>();
-        foreach (var questionResponses in responses)
+        var surveyQuestions = new List<SurveyQuestion>();
+        for (int i = 0; i < responses.Count; i++)
         {
+            var questionResponses = responses[i];
+
             if (questionResponses.Count == 0)
             {
-                topAnswers.Add(new List<string> { "" });
+                surveyQuestions.Add(new SurveyQuestion { Question = "", TopAnswers = new List<string>() });
                 continue;
             }
 
             var groupedAnswers = questionResponses.GroupBy(answer => answer)
-                                                  .OrderByDescending(group => group.Count())
-                                                  .Select(group => group.Key)
-                                                  .Take(5)
-                                                  .ToList();
-            topAnswers.Add(groupedAnswers);
+                .OrderByDescending(group => group.Count())
+                .Select(group => group.Key)
+                .Take(5)
+                .ToList();
+
+            surveyQuestions.Add(new SurveyQuestion { Question = $"Ответы на вопрос {char.ConvertFromUtf32('а' + i)}:", TopAnswers = groupedAnswers });
         }
 
-        Console.WriteLine("Ответы на вопрос а):");
-        PrintTopAnswers(topAnswers[0]);
-        Console.WriteLine("\nОтветы на вопрос б):");
-        PrintTopAnswers(topAnswers[1]);
-        Console.WriteLine("\nОтветы на вопрос в):");
-        PrintTopAnswers(topAnswers[2]);
-    }
-
-    static void PrintTopAnswers(List<string> answers)
-    {
-        if (answers.Count == 0)
+        foreach (var question in surveyQuestions)
         {
-            Console.WriteLine("Нет ответов.");
-            return;
-        }
-
-        var totalCount = answers.Count;
-        var answerCounts = answers.GroupBy(answer => answer)
-                                  .ToDictionary(group => group.Key, group => group.Count());
-
-        foreach (var answer in answers)
-        {
-            var percentage = (double)answerCounts[answer] / totalCount * 100;
-            Console.WriteLine($"Ответ: {answer}, Доля: {percentage:F2}%");
+            Console.WriteLine(question.Question);
+            foreach (var answer in question.TopAnswers)
+            {
+                Console.WriteLine(answer);
+            }
+            Console.WriteLine();
         }
     }
 }
