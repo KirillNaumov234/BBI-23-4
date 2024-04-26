70b8b224502e37ac9ed1e2d8db3762f358aef78e
KirillNaumov234 <162305953+KirillNaumov234@users.noreply.github.com>
2024-04-26 21:15:07 +0300

    Update лаба7.3.cs

--- лаба7.3.cs
+++ лаба7.3.cs
@@ -13,32 +13,71 @@ class Country
     protected void PrintSurveyResults(List<List<string>> responses)
     {
         var answerCounts = new Dictionary<string, int>();
-
-        foreach (var questionResponses in responses)
+        foreach (var answerList in responses)
         {
-            foreach (var answer in questionResponses)
+            foreach (var answer in answerList)
             {
-                if (!answerCounts.ContainsKey(answer))
+                if (answerCounts.ContainsKey(answer))
                 {
-                    answerCounts[answer] = 0;
+                    answerCounts[answer]++;
                 }
-                answerCounts[answer]++;
+                else
+                {
+                    answerCounts[answer] = 1;
+                }
             }
         }
-        var topAnswers = answerCounts.OrderByDescending(kv => kv.Value)
-                                     .Take(5);
 
-        foreach (var answer in topAnswers)
+        var totalResponses = responses.Sum(x => x.Count);
+
+        var topAnswers = answerCounts.OrderByDescending(x => x.Value)
+                                      .Take(5)
+                                      .ToDictionary(pair => pair.Key, pair => ((double)pair.Value / totalResponses) * 100);
+
+        foreach (var entry in topAnswers)
         {
-            double percentage = (double)answer.Value / responses.Sum(r => r.Count) * 100;
-            Console.WriteLine($"Answer: {answer.Key}, Percentage: {percentage:F2}%");
+            Console.WriteLine($"{entry.Key}: {entry.Value:f2}%");
         }
     }
 }
 
-class Russia : Country { }
+class Russia : Country
+{
+    public override void ProcessResponses(List<List<string>> responses)
+    {
+        Console.WriteLine("Results for Russia:");
+        base.ProcessResponses(responses);
+    }
+}
 
-class Japan : Country { }
+class Japan : Country
+{
+    public override void ProcessResponses(List<List<string>> responses)
+    {
+        Console.WriteLine("Results for Japan:");
+        base.ProcessResponses(responses);
+    }
+}
+
+class InternationalSurvey
+{
+    public List<Country> Countries { get; set; }
+
+    public InternationalSurvey(List<Country> countries)
+    {
+        Countries = countries;
+    }
+
+    public void ProcessInternationalResponses(List<List<string>> responses)
+    {
+        Console.WriteLine("International Results:");
+
+        foreach (var country in Countries)
+        {
+            country.ProcessResponses(responses);
+        }
+    }
+}
 
 class Program
 {
@@ -46,15 +85,15 @@ class Program
     {
         List<List<string>> responses = new List<List<string>>
         {
-            new List<string> { "Cat", "Dog", "Cat", "Fish" }, 
-            new List<string> { "Friendliness", "Persistence", "Hard Work", "Modesty" }, 
-            new List<string> { "Sakura", "Samurai", "Sushi", "Sake", "Sumo" } 
+            new List<string> { "Cat", "Dog", "Cat", "Fish" },
+            new List<string> { "Friendliness", "Persistence", "Hard Work", "Modesty" },
+            new List<string> { "Sakura", "Samurai", "Sushi", "Sake", "Sumo" }
         };
 
         Russia russia = new Russia();
-        russia.ProcessResponses(responses);
-
         Japan japan = new Japan();
-        japan.ProcessResponses(responses);
+
+        InternationalSurvey internationalSurvey = new InternationalSurvey(new List<Country> { russia, japan });
+        internationalSurvey.ProcessInternationalResponses(responses);
     }
 }
