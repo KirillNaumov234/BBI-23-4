2f5658177c03a198ba1429738fb52ad65fbfe177
KirillNaumov234 <162305953+KirillNaumov234@users.noreply.github.com>
2024-05-30 22:48:07 +0300

    Update Лаба7 уровень 3.cs

--- Лаба7 уровень 3.cs
+++ Лаба7 уровень 3.cs
@@ -1,33 +1,53 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 class Country
 {
     public string Name { get; set; }
-    public int AnswerCount { get; private set; }
+    public Dictionary<string, int> Answers { get; private set; }
     private static int TotalAnswerCount = 0;
 
     public Country(string name)
     {
         Name = name;
-        AnswerCount = 0;
+        Answers = new Dictionary<string, int>();
     }
 
-    public void AddAnswer()
+    public void AddAnswer(string answer)
     {
-        AnswerCount++;
+        if (Answers.ContainsKey(answer))
+        {
+            Answers[answer]++;
+        }
+        else
+        {
+            Answers[answer] = 1;
+        }
         TotalAnswerCount++;
     }
 
-    public double GetPercentage()
+    public double GetPercentage(string answer)
     {
-        if (TotalAnswerCount == 0) return 0;
-        return (double)AnswerCount / TotalAnswerCount * 100;
+        if (TotalAnswerCount == 0 || !Answers.ContainsKey(answer)) return 0;
+        return (double)Answers[answer] / TotalAnswerCount * 100;
     }
 
     public static int GetTotalAnswerCount()
     {
         return TotalAnswerCount;
+    }
+
+    public void PrintTop5Answers()
+    {
+        var sortedAnswers = Answers.OrderByDescending(a => a.Value).Take(5);
+        Console.WriteLine($"Топ-5 ответов для {Name}:");
+        foreach (var answer in sortedAnswers)
+        {
+            double percentage = (double)answer.Value / Answers.Values.Sum() * 100;
+            Console.WriteLine($"{answer.Key}: {percentage:F2}%");
+        }
+        Console.WriteLine();
     }
 }
 
@@ -50,19 +70,49 @@ class Program
 
         List<string> russianAnswers = new List<string> { "Кошка", "Собака", "Кошка", "Рыба" };
         List<string> japaneseAnswers = new List<string> { "Сакура", "Самурай", "Суши", "Саке", "Сумо" };
-
+        
         foreach (var answer in russianAnswers)
         {
-            russia.AddAnswer();
+            russia.AddAnswer(answer);
         }
 
         foreach (var answer in japaneseAnswers)
         {
-            japan.AddAnswer();
+            japan.AddAnswer(answer);
         }
 
-        Console.WriteLine($"Ответы для России: {russia.GetPercentage()}%");
-        Console.WriteLine($"Ответы для Японии: {japan.GetPercentage()}%");
-        Console.WriteLine($"Ответы для обеих стран вместе: {Country.GetTotalAnswerCount()}");
+        russia.PrintTop5Answers();
+        japan.PrintTop5Answers();
+
+        Dictionary<string, int> combinedAnswers = new Dictionary<string, int>();
+        foreach (var answer in russia.Answers)
+        {
+            if (combinedAnswers.ContainsKey(answer.Key))
+            {
+                combinedAnswers[answer.Key] += answer.Value;
+            }
+            else
+            {
+                combinedAnswers[answer.Key] = answer.Value;
+            }
+        }
+        foreach (var answer in japan.Answers)
+        {
+            if (combinedAnswers.ContainsKey(answer.Key))
+            {
+                combinedAnswers[answer.Key] += answer.Value;
+            }
+            else
+            {
+                combinedAnswers[answer.Key] = answer.Value;
+            }
+        }
+        var sortedCombinedAnswers = combinedAnswers.OrderByDescending(a => a.Value).Take(5);
+        Console.WriteLine("Топ-5 ответов для обеих стран вместе:");
+        foreach (var answer in sortedCombinedAnswers)
+        {
+            double percentage = (double)answer.Value / Country.GetTotalAnswerCount() * 100;
+            Console.WriteLine($"{answer.Key}: {percentage:F2}%");
+        }
     }
 }
